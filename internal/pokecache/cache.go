0eff0beebd22200dd6b4bc1063a656fd3ca08d32
unnxt30 <officialunnat30@gmail.com>
2024-04-02 20:31:07 +0530

    Worked on Cache

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -11,10 +11,71 @@ type cacheEntry struct {
 }
 
 type Cache struct {
-	cahceEntry_map map[string]cacheEntry
-	lock           *sync.Mutex
+	cacheEntryMap map[string]cacheEntry
+	lock          *sync.Mutex
 }
 
-func (c *Cache) Add(key string, val []byte) {
+func NewCache(interval time.Duration) Cache {
+
+	var ourCache Cache
+	ourCache.cacheEntryMap = make(map[string]cacheEntry)
+	ourCache.lock = &sync.Mutex{}
+
+	var returnCache Cache
+	go ourCache.readLoop(interval);
+	return returnCache
 
 }
+
+func (c *Cache) Add(key string, param_val []byte) {
+
+	c.lock.Lock()
+	var temp_entry cacheEntry
+	temp_entry.val = param_val
+	temp_entry.createdAt = time.Now().UTC()
+
+	c.cacheEntryMap[key] = temp_entry
+
+	c.lock.Unlock()
+
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	value, ok := c.cacheEntryMap[key]
+
+	if ok {
+		return value.val, true
+	}
+
+	return nil, false
+
+}
+
+func (c *Cache) readLoop(valid_duration time.Duration) {
+	interval := valid_duration
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	end := make(chan bool);
+
+	for {
+		select {
+			case <- ticker.C :
+				for x, y := range c.cacheEntryMap{
+					current_time := time.Now().UTC()
+					if current_time.Sub(y.createdAt) > valid_duration {
+						delete(c.cacheEntryMap, x);
+					}
+
+				}
+
+				end <- true
+				return
+	
+			}
+		}
+
+	}
